dadb26f1f01d13252cd71cccd11fff256c7cebe0
Shane Lupton <slupton89@gmail.com>
2018-10-24 11:34:20 -0600

    updated env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  API_URI: process.env.API_URI || 'http://localhost:8080/api',
-  REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api'
+  API_URI: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
 }
